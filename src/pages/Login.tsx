84668606c359008f0b1a11090c9168c812a1f540
Andrew Barker <andrewbarker96@gmail.com>
2024-06-05 11:48:45 -0500

    Create Account Setup, App.tsx, AuthApp.tsx, Main.tsx changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,9 @@ import {
   IonContent,
   IonInput,
   IonItem,
-  IonLabel,
   IonPage,
   IonText,
   IonIcon,
-  IonCardContent,
   IonModal,
   IonGrid,
   IonCol,
@@ -19,23 +17,23 @@ import {
   IonButtons,
   IonToast,
   IonImg,
+  IonInputPasswordToggle,
 } from '@ionic/react';
 import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { firebase } from '../../util/firebase';
-import { close, eye, eyeOff, send } from 'ionicons/icons';
+import { close, create } from 'ionicons/icons';
 import Copyright from '../components/Copyright';
-import { IonRefresher } from '@ionic/react';
 import { IonLoading } from '@ionic/react';
-import Logo from '../components/Logo';
+import ForgotPasswordForm from '../components/Forms/ForgotPassword';
+import CreateAccountForm from '../components/Forms/CreateAccount';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailVerification, setEmailVerification] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
   const [invalid, setInvalid] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [modal, showModal] = useState(false);
 
   const auth = getAuth(firebase);
 
@@ -57,59 +55,43 @@ export default function LoginPage() {
       });
   };
 
-  const clickForgotPassword = () => {
-    setShowModal(true);
-  }
-
-  const handleForgotPassword = () => {
-    sendPasswordResetEmail(auth, emailVerification)
-      .then(() => {
-        console.log('Password reset email sent');
-        setSuccess(true);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error('Error sending password reset email:', errorCode, errorMessage);
-        setInvalid(true);
-        setEmailVerification('');
-      });
-  }
-
 
   return (
+
     // Main Login Form
     <IonPage>
       <IonContent className='ion-padding'>
         <IonGrid className='form'>
           <IonRow>
             <IonCol size='12'>
-              <IonImg src='https://firebasestorage.googleapis.com/v0/b/skill-share-791ad.appspot.com/o/SkillSwap.png?alt=media&token=e7758927-9883-440e-b153-530f85c8d8c9' alt='SkillSwap Logo' style={{ height: '200px' }} />
+              <IonImg src='https://firebasestorage.googleapis.com/v0/b/skill-share-791ad.appspot.com/o/SkillSwap.png?alt=media&token=e7758927-9883-440e-b153-530f85c8d8c9' alt='SkillSwap Logo' style={{ height: '175px' }} />
             </IonCol>
-          </IonRow>
-          <IonRow>
             <IonCol size='12'>
-            <IonItem>
-              <IonInput type='email' labelPlacement='floating' label='Email' placeholder='Email' value={email} onIonChange={e => setEmail(e.detail.value!)} />
-            </IonItem>
-            <IonItem>
-              <IonInput type={showPassword ? 'text' : 'password'} labelPlacement='floating' label='Password' placeholder='Password' value={password} onIonChange={e => setPassword(e.detail.value!)} />
-              <IonIcon slot='end' icon={showPassword ? eye : eyeOff} size='small' color='medium' onClick={() => setShowPassword(!showPassword)} />
-            </IonItem>
-            <IonItem>
-              <IonLabel position='stacked' color='danger'>{invalid ? 'Invalid email or password' : ''}</IonLabel>
-              <IonButton slot='end' fill='clear' onClick={clickForgotPassword}>Forgot Password?</IonButton>
+              <IonItem lines='none'>
+                <IonInput type='email' labelPlacement='stacked' label='Email' placeholder='Email' value={email} onIonChange={e => setEmail(e.detail.value!)} />
               </IonItem>
             </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonButton id='open-loading' expand='block' onClick={handleLogin}>Login</IonButton>
+            <IonCol size='12'>
+              <IonItem lines='none'>
+                <IonInput type='password' labelPlacement='stacked' label='Password' placeholder='Password' value={password} onIonChange={e => setPassword(e.detail.value!)}>
+                  <IonInputPasswordToggle color={'medium'} slot='end' />
+                </IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol size='12'>
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <ForgotPasswordForm />
+              </div>
             </IonCol>
           </IonRow>
           <IonRow>
             <IonCol>
-              <IonButton color={'secondary'} expand='block' routerLink='/register'>Register</IonButton>
+              <IonButton shape='round' id='open-loading' expand='block' onClick={handleLogin}>Login</IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonButton color={'secondary'} shape='round' expand='block' onClick={() => window.location.href = '/create-account'}>Create Account</IonButton>
             </IonCol>
           </IonRow>
           <IonRow>
@@ -118,46 +100,22 @@ export default function LoginPage() {
             </IonCol>
           </IonRow>
         </IonGrid>
-        
-            <IonLoading className='custom-loading' trigger='open-loading' isOpen={false} onDidDismiss={() => setInvalid(false)} message='Logging in...' duration={2000} />
 
-
-
-
-        {/* Reset Password Modal */}
-        <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)}>
+        <IonModal isOpen={modal} onDidDismiss={() => showModal(false)}>
           <IonHeader>
             <IonToolbar>
               <IonButtons slot='start'>
-                <IonButton slot='icon-only' onClick={() => setShowModal(false)}>
+                <IonButton slot='icon-only' onClick={() => showModal(false)}>
                   <IonIcon icon={close} />
                 </IonButton>
               </IonButtons>
-              <IonTitle>Reset Password</IonTitle>
+              <IonTitle>Create Account</IonTitle>
             </IonToolbar>
           </IonHeader>
-          <IonContent className='ion-padding'>
-            <IonGrid>
-              <IonRow>
-                <IonCol size='12'>
-                  <IonText className='ion-text-center'>Please enter your email address below to receive a password reset email.</IonText>
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size='12'>
-                  <IonInput type='email' labelPlacement='floating' label='Email' placeholder='Enter Email Address' value={emailVerification} onIonChange={e => setEmailVerification(e.detail.value!)} />
-                </IonCol>
-              </IonRow>
-              <IonRow>
-                <IonCol size='12'>
-                  <IonButton expand='block' onClick={handleForgotPassword}>Send Verification Email</IonButton>
-                </IonCol>
-              </IonRow>
-              <IonToast isOpen={invalid} onDidDismiss={() => setInvalid(true)} message='Email does not exist and/or is invalid' duration={2000} />
-              <IonToast isOpen={success} onDidDismiss={() => setSuccess(false)} message='Password reset email sent. Email may take 5-10 minutes. If you do not receive contact your Stock & Associates IT Admin.' duration={5000} />
-            </IonGrid>
-          </IonContent>
+          <CreateAccountForm />
         </IonModal>
+        <IonLoading className='custom-loading' trigger='open-loading' isOpen={false} onDidDismiss={() => setInvalid(false)} message='Logging in...' duration={500} />
+        <IonToast isOpen={invalid} color={'danger'} onDidDismiss={() => setInvalid(false)} message='Invalid email or password' duration={2000} />
       </IonContent>
     </IonPage>
   );
