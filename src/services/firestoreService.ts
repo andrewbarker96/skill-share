af6a98ce1e621f0683d83d2c0871ce1b1f767772
Andrew Barker <andrewbarker96@gmail.com>
2024-06-14 16:04:10 -0500

    -

--- src/services/firestoreService.ts
+++ src/services/firestoreService.ts
@@ -1,23 +1,46 @@
-import { db } from '../../util/firebase';
-import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc, setDoc, getDoc } from "firebase/firestore";
-import { ProfileData, Skills } from '../types';
+import { db } from "../../util/firebase";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import { ProfileData, Skills } from "../types";
 
 //Create new profile
 export const createProfile = async (profile: any) => {
-    try {
-        const profileDoc = doc(db, "userProfiles", profile.uid);
-        await setDoc(profileDoc, profile);
-        return profile.uid;
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
+  try {
+    const profileDoc = doc(db, "userProfiles", profile.uid);
+    await setDoc(profileDoc, profile);
+    return profile.uid;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
 };
 
 //Get all profiles
 export const getProfiles = async () => {
-    const querySnapshot = await getDocs(collection(db, "userProfiles"));
-    return querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-}
+  const querySnapshot = await getDocs(collection(db, "userProfiles"));
+  return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+};
+
+// Function to get user profile data from Firestore
+export const getUserProfile = async (uid: string): Promise<ProfileData> => {
+  if (!uid) throw new Error("No user ID provided");
+
+  const userDoc = doc(db, "userProfiles", uid);
+  const userSnapshot = await getDoc(userDoc);
+
+  if (!userSnapshot.exists()) {
+    throw new Error("User profile does not exist");
+  }
+
+  return userSnapshot.data() as ProfileData;
+};
 
 //Update a profile
 export const updateProfile = async (id: string, updatedProfile: any) => {
@@ -32,9 +55,17 @@ export const deleteProfile = async (id: string) => {
 };
 
 //Get all skills
-export const getSkills = async () => {
-    const querySnapshot = await getDocs(collection(db, "skills"));
-    return querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+export const getSkills = async (): Promise<Skills> => {
+  const skillsDoc = doc(db, "skills", "471rn4UyI8WDOGW3R15N");
+  const skillsSnapshot = await getDoc(skillsDoc);
+
+  if (!skillsSnapshot.exists()) {
+    throw new Error("Skills document does not exist");
+  }
+
+  const data = skillsSnapshot.data() as Skills;
+  console.log("fetched skills data: ", data);
+  return data;
 };
 
 //Add new skill
