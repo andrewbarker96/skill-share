f0287717b13a615ddb963bcbca59978019bfa69d
kaylavera99 <kaylavd99@gmail.com>
2024-06-26 21:35:51 -0400

    Fixed chat scroll
    When user goes to their chat thread, they are brought to the latest chat at the bottom.

--- src/components/Messaging/chat/chat.tsx
+++ src/components/Messaging/chat/chat.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./chat.css";
-import { Timestamp } from 'firebase/firestore';
 import {
   IonButton,
   IonButtons,
@@ -8,22 +7,20 @@ import {
   IonContent,
   IonIcon,
   IonInput,
-  IonItem,
   IonPage,
   IonPopover,
   IonRow,
   IonText,
-  IonLabel,
-  IonItemDivider,
 } from "@ionic/react";
-import { camera, checkmarkOutline, happy, send, checkmarkSharp, checkmark, checkmarkDone, checkmarkDoneCircle, checkmarkCircle } from "ionicons/icons";
+import { camera, checkmark, happy, send, checkmarkCircle } from "ionicons/icons";
 import EmojiPicker from "emoji-picker-react";
 import {
   sendMessage,
   getMessages,
   getChat,
   updateMessageStatusToRead,
-  onMessagesSnapshot
+  onMessagesSnapshot,
+  updateLastSentMessageStatus,
 } from "../../../services/messageService";
 import UserInfo from "../list/userInfo/UserInfo";
 import { auth } from "../../../../util/firebase";
@@ -41,10 +38,12 @@ const IndividualChat: React.FC = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
   const [chatId, setChatId] = useState<string>("");
-  const [messages, setMessages] = useState<any[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
   const [targetUserId, setTargetUserId] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   const [profileImage, setProfileImage] = useState<string>("");
+
+  const contentRef = useRef<HTMLIonContentElement>(null);
 
   useEffect(() => {
     const url = window.location.href;
@@ -55,36 +54,34 @@ const IndividualChat: React.FC = () => {
     if (chatIdFromUrl) {
       const fetchChatData = async () => {
         try {
-          // Fetch chat details to get target user ID
           const chat = await getChat(chatIdFromUrl);
           const currentUserId = auth.currentUser?.uid;
           if (chat && chat.users && currentUserId) {
-            const otherUserId = chat.users.find(
-              (id: string) => id !== currentUserId
-            );
+            const otherUserId = chat.users.find((id: string) => id !== currentUserId);
             if (otherUserId) {
               setTargetUserId(otherUserId);
-
-              // Fetch target user's profile to get the username
               const userProfile = await getUserProfile(otherUserId);
-              // console.log("Fetched user data: ", userProfile);
               setUsername(userProfile.username);
               setProfileImage(userProfile.profileImage);
-              // console.log("Profile Picture URL:", userProfile.profileImage);
             }
 
-            // Set up real-time listener for messages
             const unsubscribe = onMessagesSnapshot(chatIdFromUrl, async (messages) => {
               setMessages(messages);
+              scrollToBottom();
 
               for (const message of messages) {
-                if (
-                  message.senderId !== currentUserId &&
-                  message.status === "delivered"
-                ) {
+                if (message.senderId !== currentUserId && message.status === "delivered") {
                   await updateMessageStatusToRead(chatIdFromUrl, message.id);
                 }
-              };
+              }
+
+              // Update the last sent message status to 'receivedReply' when a new message is received
+              const newMessageFromOtherUser = messages.find(
+                (message) => message.senderId !== currentUserId && message.status === 'sent'
+              );
+              if (newMessageFromOtherUser) {
+                await updateLastSentMessageStatus(chatIdFromUrl, currentUserId);
+              }
             });
 
             return () => unsubscribe();
@@ -108,59 +105,65 @@ const IndividualChat: React.FC = () => {
       setText("");
       const messages = await getMessages(chatId);
       setMessages(messages);
+      scrollToBottom();
     } catch (error) {
       console.error("Error sending message:", error);
     }
   };
+
+  const scrollToBottom = () => {
+    if (contentRef.current) {
+      contentRef.current.scrollToBottom(300);
+    }
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(scrollToBottom, 100); // Give some time for rendering
+    return () => clearTimeout(timeout);
+  }, [messages]);
 
   const currentUserId = auth.currentUser?.uid;
 
   return (
     <IonPage>
       <div className="top">
-        <UserInfo username={username} profilePicture={profileImage} />{" "}
-        {/* Pass the target user's username */}
+        <UserInfo username={username} profilePicture={profileImage} />
       </div>
-      <IonContent scrollY={true} id="texts">
+      <IonContent ref={contentRef} scrollY={true} id="texts">
         {messages.map((message) => {
           const messageClass =
             message.senderId === currentUserId ? "message-sender" : "message-target";
-
           const textClass =
             message.senderId === currentUserId ? "message-sender-text" : "message-target-text";
 
           return (
-            <div
-              className={`message ${messageClass}`}
-              key={message.id}
-
-            >
+            <div className={`message ${messageClass}`} key={message.id}>
               <IonText className={textClass}>{message.message}</IonText>
               <IonText>
                 {messageClass === "message-sender" && message.status !== 'receivedReply' && (
-                  <IonText color={'medium'} className="message-status ">
-                    {message.status === 'sent' &&
+                  <IonText color={'medium'} className="message-status">
+                    {message.status === 'sent' && (
                       <>
                         <IonText>Sent</IonText>
                         <IonIcon icon={checkmark} />
                       </>
-                    }
-                    {message.status === 'delivered' &&
+                    )}
+                    {message.status === 'delivered' && (
                       <>
                         <IonText color={'primary'}>Delivered</IonText>
                         <IonIcon icon={checkmark} />
-                      </>}
-                    {message.status === 'read' &&
+                      </>
+                    )}
+                    {message.status === 'read' && (
                       <>
                         <IonText>Read</IonText>
                         <IonIcon icon={checkmarkCircle} />
                       </>
-                    }
+                    )}
                   </IonText>
                 )}
               </IonText>
             </div>
-
           );
         })}
       </IonContent>
