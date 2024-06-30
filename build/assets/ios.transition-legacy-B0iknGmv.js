c3a7cde016c18bfaa97f29cd8ecc0359b242aa52
Andrew Barker <andrewbarker96@gmail.com>
2024-06-30 12:44:52 -0500

    Build/Sync

--- build/assets/ios.transition-legacy-B0iknGmv.js
+++ build/assets/ios.transition-legacy-B0iknGmv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DPBfQ8Oh.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
+System.register(["./index-legacy-DMExqHyI.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.v,n=t.x}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
